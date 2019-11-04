2c5ba2e21ab19a97ca5cc60c230cf3b63c8ba670
Eugeniu Stina <estina@c2r12s2.42madrid.com>
2019-11-04 19:52:10 +0100

    strlcat done

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estina <estina@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 19:30:32 by estina            #+#    #+#             */
+/*   Updated: 2019/11/04 19:49:15 by estina           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strlcat(char *dst, const char *src, int dstsize)
+{
+	int	i;
+	int	j;
+	int	res;
+
+	i = 0;
+	while (dst[i] != '\0')
+		++i;
+	res = 0;
+	while (src[res] != '\0')
+		++res;
+	if (dstsize <= i)
+		res += dstsize;
+	else
+		res += i;
+	j = 0;
+	while (src[j] != '\0' && i + 1 < dstsize)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (res);
+}
