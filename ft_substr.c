758fb5510e82d7647f6991d17d06e21765a198bd
Eugeniu Stina <estina@c2r12s2.42madrid.com>
2019-11-06 17:15:38 +0100

    substr corrected

--- ft_substr.c
+++ ft_substr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: estina <estina@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 13:06:37 by estina            #+#    #+#             */
-/*   Updated: 2019/11/05 13:12:27 by estina           ###   ########.fr       */
+/*   Updated: 2019/11/06 17:15:04 by estina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*dst;
 	size_t	i;
 
-	dst = malloc(len);
+	dst = malloc(len * sizeof(dst));
 	i = 0;
+	if (ft_strlen(s) < start)
+		while (i++ <= len)
+			dst[i - 1] = 0;
 	while (i < len)
 	{
 		dst[i] = s[start + i];
