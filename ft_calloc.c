b94e290476df20ab8dbe0dc86d99959acf842dab
Eugeniu Stina <estina@c2r12s2.42madrid.com>
2019-11-06 15:02:10 +0100

    calloc corrected

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: estina <estina@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 23:40:04 by estina            #+#    #+#             */
-/*   Updated: 2019/11/05 12:50:55 by estina           ###   ########.fr       */
+/*   Updated: 2019/11/06 15:01:45 by estina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_calloc(size_t count, size_t size)
 		return (NULL);
 	p = malloc(count * size);
 	i = 0;
-	while (i <= count)
+	while (i < count)
 	{
 		((char*)p)[i] = 0;
 		i++;
