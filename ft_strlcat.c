70fe32e607a5a7a908dac45823503aaa856d72db
Eugeniu Stina <estina@c2r12s2.42madrid.com>
2019-11-06 16:05:49 +0100

    strlcat corrected

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: estina <estina@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 19:30:32 by estina            #+#    #+#             */
-/*   Updated: 2019/11/04 20:51:57 by estina           ###   ########.fr       */
+/*   Updated: 2019/11/06 16:05:33 by estina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	dst_len;
+	size_t	len;
 	size_t	i;
 	size_t	j;
-	size_t	res;
 
-	i = 0;
-	while (dst[i] != '\0')
-		++i;
-	res = 0;
-	while (src[res] != '\0')
-		++res;
-	if (dstsize <= i)
-		res += dstsize;
-	else
-		res += i;
-	j = 0;
-	while (src[j] != '\0' && i + 1 < dstsize)
+	dst_len = ft_strlen(dst);
+	if (dstsize < dst_len)
+		dst_len = dstsize;
+	len = dstsize - dst_len;
+	if (!len)
 	{
-		dst[i] = src[j];
+		return (dst_len + ft_strlen(src));
+	}
+	i = 0;
+	j = dst_len;
+	while (src[i] && j + 1 < dstsize)
+	{
+		dst[j] = src[i];
 		i++;
 		j++;
 	}
-	dst[i] = '\0';
-	return (res);
+	dst[j] = 0;
+	return (dst_len + ft_strlen(src));
 }
